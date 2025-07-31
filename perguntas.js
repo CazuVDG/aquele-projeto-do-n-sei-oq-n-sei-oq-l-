ab8a8e623a2f895366520d0679f3b4fb493626ac
Cazu Booby Goods da Silva <luiz.cazuza.silva@escola.pr.gov.br>
2025-07-31 11:03:29 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,27 @@
-
+const perguntas = [
+    {
+    categoria: "[geografia]",
+    pergunta: "[Qual é o menor país do mundo? ]",
+    resposta: "[Vaticano]"
+    },
+    {
+    categoria: "[matemática]",
+    pergunta: "[O triangulo equilatero é aquele que:]",
+    resposta: "[possui todos os três lados com a mesma medida]"
+    },
+    {
+    categoria: "[biologia]",
+    pergunta: "[o ornitorinco é um animal de qual classe?]",
+    resposta: "[mamífero]"
+    },
+    {
+    categoria: "[historia]",
+    pergunta: "[qual a data do descobrimento do Brasil?]",
+    resposta: "[22 de abril de 1500]"
+    },
+    {
+    categoria: "[química]",
+    pergunta: "[Quais são as ligações químicas]",
+    resposta: "[Resposta 5 do Aluno]"
+    }
+    ];
